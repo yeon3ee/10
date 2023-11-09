bcf34f2dbc5998d642044e6c80e946eda33a09da
yeon3ee <2310538@sookmyung.ac.kr>
2023-11-09 16:50:06 +0900

    05

--- main.c
+++ main.c
@@ -2,8 +2,22 @@
 #include <string.h>
 
 int main(void) {
-    char str[30] = "happy C programming";
-    printf("문자열 \"%s\"의 길이 : %i", str, strlen(str));
-    
-    return 0;
+   FILE* fp;
+   char str[100];
+   int i;
+   fp=fopen("sample.txt","w");
+   
+   for (i=0;i<3;i++);
+   {
+  	printf("input a word");
+   	scanf("%s",str); //&필요없음. 자체적으로 포인터
+  	fprintf(fp,"%s\n",str); 
+   
+	}
+   
+   fclose(fp);
+   
+   return 0;
 }
+
+   
